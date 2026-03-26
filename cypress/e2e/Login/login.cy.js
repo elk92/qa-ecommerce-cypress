fdbf8ef4cc59b3262187be4cab5ebcf8159c4ade
Elcio Filho <elcio.filho@hypera.com.br>
2026-03-26 17:02:40 -0300

    test(login): add before each and adapt login to use commands

--- cypress/e2e/Login/login.cy.js
+++ cypress/e2e/Login/login.cy.js
@@ -1,25 +1,27 @@
-/// reference types = "cypress" />
+/// <reference types="cypress" />
 
-describe('Login funcional de login', () => {
-    it('Login on saucedemon with success', () => {
-        cy.visit('https://www.saucedemo.com/');
-        cy.get('#user-name').type('standard_user');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
-        cy.url().should('include', '/inventory.html');
-    });
-    it('Login on saucedemo with incorrect username', () => {
-        cy.visit('https://www.saucedemo.com/');
-        cy.get('#user-name').type('standard_user');
-        cy.get('#password').type('wrong_password');
-        cy.get('#login-button').click();
-        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
-    });
-    it('Login on saucedemo with incorrect password', () => {
-        cy.visit('https://www.saucedemo.com/');
-        cy.get('#user-name').type('wrong_user');
-        cy.get('#password').type('secret_sauce');
-        cy.get('#login-button').click();
-        cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
-    });
+describe('Login - SauceDemo', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/');
   });
+
+  it('should login successfully with valid credentials', () => {
+    cy.login('standard_user', 'secret_sauce');
+    cy.url().should('include', '/inventory.html');
+  });
+
+  it('should show error message with invalid username', () => {
+    cy.login('wrong_user', 'secret_sauce');
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Username and password do not match');
+  });
+
+  it('should show error message with invalid password', () => {
+    cy.login('standard_user', 'wrong_password');
+    cy.get('[data-test="error"]')
+      .should('be.visible')
+      .and('contain', 'Username and password do not match');
+  });
+});
